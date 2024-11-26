a565a45c4a81afc9e45b65b8f26eb1c3641d5bd7
travis h <travis.a.humphreys@gmail.com>
2024-11-26 16:14:51 -0600

    attempting to solve architechture error

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./nixos/configuration.nix
+        # inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
