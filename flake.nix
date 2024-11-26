1f7f2a099f22d75cc75df925673aaf3fbac8209a
travis h <travis.a.humphreys@gmail.com>
2024-11-26 17:27:59 -0600

    IT BUILDS

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+    nixosConfigurations."thinkpad"= nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = {inherit inputs;};
       modules = [
