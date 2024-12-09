c9c1575d9347788e86b2c291b7f3f30c3770b223
travis h <travis.a.humphreys@gmail.com>
2024-12-08 19:02:22 -0600

    flake updated, added keybinds, preparing for switch to flake hyprland

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -37,6 +37,10 @@
   networking.networkmanager.enable = true;
 
   nix.extraOptions = ''experimental-features = nix-command flakes'';
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
 
   services.displayManager.ly.enable = true;
   services.displayManager.ly.settings = {animation = "doom";};
